a1ab7acea3a4f1bee2f0d4f12852183a5f963ce0
shengxiang <hzsunshx@corp.netease.com>
2015-10-11 22:44:13 +0800

    add breadcrumb

--- public/explorer.jsx
+++ public/explorer.jsx
@@ -10,109 +10,121 @@ var Button = require('react-bootstrap').Button;
 var ButtonToolbar = require('react-bootstrap').ButtonToolbar;
 var DropdownButton = require('react-bootstrap').DropdownButton;
 var MenuItem = require('react-bootstrap').MenuItem;
+var Modal = require('react-bootstrap').Modal;
+var ReactBS = require('react-bootstrap');
+var Row = ReactBS.Row,
+  Col = ReactBS.Col,
+  Breadcrumb = ReactBS.Breadcrumb,
+  BreadcrumbItem = ReactBS.BreadcrumbItem;
 
+var path = require('path')
+
+var FileItem = require('./file-item.jsx')
 
 var fileList = [
-	{
-		name: "hello.js",
-		type: "directory",
-		size: 1323003,
-		mtime: 12283844
-	},
-	{
-		name: "world.md",
-		type: "file",
-		size: 13322,
-		mtime: 122123123844
-	}
+  {
+    name: "hello.js",
+    type: "directory",
+    size: 1323003,
+    mtime: 12283844
+  },
+  {
+    name: "world.md",
+    type: "file",
+    size: 13322,
+    mtime: 122123123844
+  }
 ]
 
-var FileItem = React.createClass({
-	render: function(){
-		var fileType = this.props.data.type;
-		var fileIcon;
-		console.log(fileType);
-		if (fileType == "directory"){
-			console.log("Is dir")
-			fileIcon = <i className="fa fa-folder-open"/>;
-		} else {
-			fileIcon = <i className="fa fa-file-o"/>;
-		}
-		return (
-			<tr>
-				<td>
-					{fileIcon}
-				</td>
-				<td>{this.props.data.name}</td>
-				<td>{humanize.filesize(this.props.data.size)}</td>
-				<td>
-					<ButtonToolbar>
-						<Button bsSize="xsmall">
-							Download <i className="fa fa-download"/>
-						</Button>
-						<Button bsSize="xsmall">
-							QRCode <i className="fa fa-qrcode"/>
-						</Button>
-					</ButtonToolbar>
-				</td>
-				<td>
-					{humanize.date('Y-m-d H:i:s', this.props.data.mtime)}
-				</td>
-			</tr>
-		)
-	}
+var FileList = React.createClass({
+  render: function(){
+    var fileItems = this.props.data.map(function(item){
+      return (
+        <FileItem key={item.name} data={item}/>
+      )
+    })
+    return (
+      <tbody>
+        {fileItems}
+      </tbody>
+    )
+  }
 })
 
-var FileList = React.createClass({
-	render: function(){
-		var fileItems = this.props.data.map(function(item){
-			return (
-				<FileItem key={item.name} data={item}/>
-			)
-		})
-		return (
-			<tbody>
-				{fileItems}
-			</tbody>
-		)
-	}
+var PathBreadcrumb = React.createClass({
+  render: function(){
+    var paths = location.pathname.split('/');
+    paths.pop();
+    paths.shift();
+    console.log(paths);
+    var itemPaths = [];
+    var currPath = '/';
+    paths.forEach(function(name){
+      // var currPath = path.join(, 's')
+      var newPath = path.join(currPath, name);
+      currPath = newPath;
+      itemPaths.push({
+        directory: currPath,
+        name: name
+      })
+      console.log(currPath)
+    })
+    var items = itemPaths.map(function(subPath){
+      return (
+        <BreadcrumbItem key={subPath.directory} href={subPath.directory}>
+          {subPath.name}
+        </BreadcrumbItem>
+      )
+    })
+    return (
+      <Breadcrumb>
+        <BreadcrumbItem href="/">
+          $
+        </BreadcrumbItem>
+        {items}
+      </Breadcrumb>
+    )
+  }
 })
 
 var Explorer = React.createClass({
-	getInitialState: function(){
-		return {data: fileList}
-	},
-	render: function(){
-		return (
-			<div>
-				<ButtonToolbar>
-					<Button bsSize="small">
-						Up <i className="fa fa-level-up"/>
-					</Button>
-					<Button bsSize="small">
-						Upload <i className="fa fa-upload"/>
-					</Button>
-					<Button bsStyle="link" bsSize="small">
-						<span>{location.pathname}</span>
-					</Button>
-				</ButtonToolbar>
-				<br/>
-				<Table striped bordered condensed hover>
-					<thead>
-						<tr>
-							<th>#</th>
-							<th className="table-name">Name</th>
-							<th>Size</th>
-							<th>Control</th>
-							<th>Modity Time</th>
-						</tr>
-					</thead>
-						
-					<FileList data={this.state.data} />
-				</Table>
-			</div>
-		)
-	}
+  getInitialState: function(){
+    return {data: fileList}
+  },
+  render: function(){
+    return (
+      <Row>
+        <Col md={3}>
+          <ButtonToolbar>
+            <Button bsSize="small">
+              Up <i className="fa fa-level-up"/>
+            </Button>
+            <Button bsSize="small">
+              Upload <i className="fa fa-upload"/>
+            </Button>
+          </ButtonToolbar>
+        </Col>
+        <Col md={9}>
+          <PathBreadcrumb/>
+        </Col>
+        <Col md={12}>
+          <Table striped bordered condensed hover>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th className="table-name">Name</th>
+                <th>Size</th>
+                <th>Control</th>
+                <th>Modity Time</th>
+              </tr>
+            </thead>
+              
+            <FileList data={this.state.data} />
+          </Table>
+        </Col>
+      </Row>
+    )
+  }
 })
 
 module.exports = Explorer
---
+++ public/file-item.jsx
@@ -0,0 +1,75 @@
+var humanize = require('humanize')
+var React = require('react')
+var Button = require('react-bootstrap').Button;
+var ButtonToolbar = require('react-bootstrap').ButtonToolbar;
+var DropdownButton = require('react-bootstrap').DropdownButton;
+var MenuItem = require('react-bootstrap').MenuItem;
+var Modal = require('react-bootstrap').Modal;
+
+
+var FileItem = React.createClass({
+	getInitialState: function(){
+		return {show: false};
+	},
+	render: function(){
+		var fileType = this.props.data.type;
+		var fileIcon;
+		console.log(fileType);
+		if (fileType == "directory"){
+			console.log("Is dir")
+			fileIcon = <i className="fa fa-folder-open"/>;
+		} else {
+			fileIcon = <i className="fa fa-file-o"/>;
+		}
+		var that = this;
+		var close = function(){
+			that.setState({show: false})
+		}
+		var open = function(){
+			that.setState({show: true})
+		}
+		return (
+			<tr>
+				<td>
+					{fileIcon}
+				</td>
+				<td>{this.props.data.name}</td>
+				<td>{humanize.filesize(this.props.data.size)}</td>
+				<td>
+					<div>
+						<ButtonToolbar>
+							<Button bsSize="xsmall" href={this.props.data.name}>
+								Download <i className="fa fa-download"/>
+							</Button>
+							<Button bsSize="xsmall" onClick={open}>
+								QRCode <i className="fa fa-qrcode"/>
+							</Button>
+						</ButtonToolbar>
+						<Modal
+							bsSize="small"
+							show={this.state.show}
+							onHide={close}
+						>
+							<Modal.Header closeButton>
+            		<Modal.Title className="text-center">{this.props.data.name}</Modal.Title>
+            		<Modal.Body>
+            			<div className="text-center">
+            				<img alt='qrcode' src={'/_qr?text='+location.href+this.props.data.name} />
+            			</div>
+            		</Modal.Body>
+            		<Modal.Footer>
+            			<Button onClick={close}>Close</Button>
+          			</Modal.Footer>
+          		</Modal.Header>
+						</Modal>
+					</div>
+				</td>
+				<td>
+					{humanize.date('Y-m-d H:i:s', this.props.data.mtime)}
+				</td>
+			</tr>
+		)
+	}
+})
+
+module.exports = FileItem;
