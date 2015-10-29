68e41b2f6153099471cb1ace33a020c2c9b1cb86
shengxiang <hzsunshx@corp.netease.com>
2015-10-29 23:30:58 +0800

    change format

--- routers/dir.go
+++ routers/dir.go
@@ -11,22 +11,6 @@ import (
 	"gopkg.in/macaron.v1"
 )
 
-func formatSize(file os.FileInfo) string {
-	if file.IsDir() {
-		return "-"
-	}
-	size := file.Size()
-	switch {
-	case size > 1024*1024:
-		return fmt.Sprintf("%.1fM", float64(size)/1024/1024)
-	case size > 1024:
-		return fmt.Sprintf("%.1fK", float64(size)/1024)
-	default:
-		return strconv.Itoa(int(size))
-	}
-	return ""
-}
-
 func deepPath(basedir, name string) string {
 	isDir := true
 	for isDir {
---
+++ routers/utils.go
@@ -0,0 +1,23 @@
+package routers
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func formatSize(file os.FileInfo) string {
+	if file.IsDir() {
+		return "-"
+	}
+	size := file.Size()
+	switch {
+	case size > 1024*1024:
+		return fmt.Sprintf("%.1fM", float64(size)/1024/1024)
+	case size > 1024:
+		return fmt.Sprintf("%.1fK", float64(size)/1024)
+	default:
+		return strconv.Itoa(int(size))
+	}
+	return ""
+}
